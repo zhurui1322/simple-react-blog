a8c8d1c487e4271bbcbde374ad7c3dcef03a24f0
Raymond Zhu <ruzhu@syr.edu>
2017-08-06 00:13:21 -0400

    bad route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import Home from './components/home';
+import Main from './components/main';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-        Hello
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <Home/>
+        <Main />
+      </MuiThemeProvider>
     );
   }
 }
