01660e94c7fbf3f0fa12ed2b048bf8fd341d8162
Raymond Zhu <ruzhu@syr.edu>
2017-08-06 13:13:58 -0400

    good route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-        Hello
-        </p>
-      </div>
-    );
-  }
-}
+const Home = () => <h1>Home</h1>;
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={Home} />
+    </div>
+  </BrowserRouter>
+)
 
 export default App;
